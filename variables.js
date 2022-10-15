5ff0c3376c25fdd4fa1ca1e1c1a39f8c393e458d
Katherine Blaine <blaine.katherine12@gmail.com>
2022-10-15 11:46:30 -0600

    Add boolean variables

--- variables.js
+++ variables.js
@@ -1,3 +1,7 @@
 var name = "Katherine"
 var email = "blaine.katherine12@gmail.com"
 var dob = "9/14/99"
+
+var assignment2Completed = false
+var assignment1Completed = true
+var breakfastFinished = true
