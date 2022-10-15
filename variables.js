b3edf7f73263aa9328ca9b03628bd2038d0ad0da
Katherine Blaine <blaine.katherine12@gmail.com>
2022-10-15 11:59:24 -0600

    Add console log

--- variables.js
+++ variables.js
@@ -9,3 +9,14 @@ var breakfastFinished = true
 var age = 23
 var pets = 0
 var cars = 1
+
+var name = "Katherine Blaine"
+var email = "blain064@umn.edu"
+var dob = "9/14/1999"
+
+console.log(age)
+console.log(name)
+console.log(dob)
+console.log(email)
+console.log(pets)
+
